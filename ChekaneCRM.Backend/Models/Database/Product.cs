8d2fed1d29daa0d406ca13b75766fcd923bdc56f
zxc c++ <93994876+AlexWify@users.noreply.github.com>
2026-05-08 16:00:22 +0300

    добавление полей для бжу и состава

--- ChekaneCRM.Backend/Models/Database/Product.cs
+++ ChekaneCRM.Backend/Models/Database/Product.cs
@@ -8,5 +8,12 @@ namespace ChekaneCRM.Backend.Models.Database
         public decimal Price { get; set; }
         public string Category { get; set; } = string.Empty;
         public bool IsAvailable { get; set; } = true;
+        
+       
+        public double Proteins { get; set; } = 0;      // Белки
+        public double Fats { get; set; } = 0;          // Жиры
+        public double Carbohydrates { get; set; } = 0; // Углеводы
+        public int Calories { get; set; } = 0;         // Ккал
+        public string Ingredients { get; set; } = string.Empty; // Состав
     }
 }
