e095a54df41e5c9ebfc2fb88fb26d00dab357da5
zxc c++ <93994876+AlexWify@users.noreply.github.com>
2026-05-11 14:25:20 +0300

    не локальная бд

--- ChekaneCRM.Backend/Program.cs
+++ ChekaneCRM.Backend/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseUrls("http://localhost:5000");
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers()
     .AddNewtonsoftJson(options =>
